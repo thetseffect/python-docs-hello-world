2fecb6d1887eb3a7b7f2d7baf4682f3d071d4f23
Tarun Singh <thetseffect@gmail.com>
2019-10-19 21:16:59 +0530

    Edit

--- application.py
+++ application.py
@@ -1,6 +1,18 @@
-from flask import Flask
+from predictor import EnsembleBuilder
+from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "go to /predict"
+
+@app.route('/predict', methods = ['GET', 'POST'])
+def predictor():
+        if(request.method == 'POST'):
+                rec_json = request.get_json()
+                recogspeech = rec_json['speech']
+                print(recogspeech)
+                eb = EnsembleBuilder()
+                result = eb.make_prediction(recogspeech)
+                print(result, recogspeech)
+                return jsonify({'sentiment' : result, 'speech': recogspeech}), 201
